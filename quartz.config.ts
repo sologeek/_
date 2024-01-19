efe3d12b92ae8d4bc6c99d8396d856087a331f57
sologeek <liyoushan3@gmail.com>
2024-01-20 00:44:13 +0800

    Quartz sync: Jan 20, 2024, 12:44 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "/",
+    baseUrl: "sologeek.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
